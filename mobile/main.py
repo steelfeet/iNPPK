21c07f227c9776dfcf64b5dc1910d8e74bfd533a
steelfeetman@gmail.com <steelfeetman@gmail.com>
2021-04-08 22:17:34 +0300

    наброски рекомендаций action, read

--- mobile/main.py
+++ mobile/main.py
@@ -127,7 +127,7 @@ class DishList(Screen):
         self.ids.calories_color_butt.md_bg_color = r, g, b, 1
 
 
-        #рекомендациия
+        #рекомендация
         db_request = {}
         db_request['tm_id'] = self._app.user_data["tm_id"]
         db_request['vk_id'] = self._app.user_data["vk_id"]
@@ -253,11 +253,25 @@ class ReadList(Screen):
             item = items_list_now[0]
             self.ids.title_1.text = item['data_4']
             self.ids.href_1.text = item['data_3']
-        
-
         except:
             pass
 
+        #рекомендация
+        db_request = {}
+        db_request['tm_id'] = self._app.user_data["tm_id"]
+        db_request['vk_id'] = self._app.user_data["vk_id"]
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'}
+        db_json = json.dumps(db_request)
+        page_url = "https://steelfeet.ru/app/reccom_read.php?q=" + db_json
+        print("запрос рекомендации: " + page_url)
+        t = requests.get(page_url, headers = headers)
+            
+        try:
+            item = json.loads(t.text)
+            self.ids.title_1.title_reccom = item['data_4']
+            self.ids.href_1.title_reccom = item['data_3']
+        except Exception as e:
+            print(traceback.format_exc())
 
     def add_href(self):
         href_text = self.ids.href_text.text
@@ -320,6 +334,24 @@ class ActionList(Screen):
             self.ids.title_5.text = dt_object.strftime("%d %b %H:%M") + " - " + item['data_3']
         except:
             pass
+
+        
+        #рекомендация
+        db_request = {}
+        db_request['tm_id'] = self._app.user_data["tm_id"]
+        db_request['vk_id'] = self._app.user_data["vk_id"]
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'}
+        db_json = json.dumps(db_request)
+        page_url = "https://steelfeet.ru/app/reccom_action.php?q=" + db_json
+        print("запрос рекомендации: " + page_url)
+        t = requests.get(page_url, headers = headers)
+            
+        try:
+            item = json.loads(t.text)
+            self.ids.title_reccom.text = dt_object.strftime("%d %b %H:%M") + " - " + item['data_3']
+        except Exception as e:
+            print(traceback.format_exc())
+
 
 
     def add_action(self):
