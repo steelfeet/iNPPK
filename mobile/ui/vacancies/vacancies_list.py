5bf9d3aeb3a9e07a8474c033858d78e106d75ffe
steelfeetman@gmail.com <steelfeetman@gmail.com>
2021-10-15 11:21:55 +0300

    mobile: v.0.3.2 [build 0028]

--- mobile/ui/vacancies/vacancies_list.py
+++ mobile/ui/vacancies/vacancies_list.py
@@ -88,7 +88,69 @@ class VacanciesList(Screen):
 
         self.ids.title_4.text = good_titles[4]
         #self.ids.href_4.text = vacancies[4]["href"]
+
+
+
+
+
+        db_request = {}
+        db_request['action'] = "show"
+        db_request['tm_id'] = self._app.user_data["tm_id"]
+        db_request['vk_id'] = self._app.user_data["vk_id"]
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0'}
+        db_json = json.dumps(db_request)
+        db_json = db_json.replace(" ", "")
+        print(db_json)
         
+        #отдельный бекенд для вакансий, т.к. он на Python для поиска подходящих вакансий
+        page_url = "https://studs.steelfeet.ru/_hack/2020-21/world-it-planet/847-hh-scrapper/last_vacancies.wsgi?q=" + str(db_json)
+        print(page_url)
+        t = requests.get(page_url, headers = headers)
+        print(t.text)
+        items_list = json.loads(t.text)
+        vacancies = items_list["vacancies"]
+        print("last vacancies:")
+        print(vacancies)
+        
+        #разбиваем title на две строчки
+        for item in vacancies:
+            words = item["title"].split(" ")
+            
+            good_title = ""
+            next_br = True
+            for word in words:
+                good_title = good_title + word + " "
+                if (len(good_title) > 20):
+                    if (next_br):
+                        good_title = good_title + "\n"
+                        next_br = False
+                        if (len(good_title) > 40):
+                            #good_title = re.sub(r'[а-я]+\s?', '',good_title).strip()
+                            break
+            
+            good_titles.append(good_title.strip())
+            titles_id[n] = item["id"]
+            n = n + 1
+
+
+        
+        #выводим на активити
+        self.ids.title_5.text = good_titles[5]
+        #self.ids.href_0.text = vacancies[0]["href"]
+
+        self.ids.title_6.text = good_titles[6]
+        #self.ids.href_1.text = vacancies[1]["href"]
+
+        self.ids.title_7.text = good_titles[7]
+        #self.ids.href_2.text = vacancies[2]["href"]
+
+        self.ids.title_8.text = good_titles[8]
+        #self.ids.href_3.text = vacancies[3]["href"]
+
+        self.ids.title_9.text = good_titles[9]
+        #self.ids.href_4.text = vacancies[4]["href"]
+
+
 
     def like_vacancy(self, title_n):
         global titles_id
